62004f487c2aa93ce2582271f7339f355ebd4c1f
WenJinGuo <13750712917@163.com>
2022-04-28 19:52:56 +0800

    生成Apple密码

--- src/密码生成器.py
+++ src/密码生成器.py
@@ -2,14 +2,17 @@
 # -*- coding: utf-8 -*-
 # @Author:文晋国
 # @Time  :2020/3/31 11:23
-import random # 随机模块
+import random  # 随机模块
+
 a = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,.?%$@!'
 b = ''
-c = random.randint(10,15) # random.randint 从指定的范围内随机产生一个数，包括上下限
+c = random.randint(10, 15)  # random.randint 从指定的范围内随机产生一个数，包括上下限
+
 for i in range(c):
-    d = random.randint(0,len(a)-1)
-    b = b + a[d]
-print(f"密码长度为：{c}")
-print(b)
+    d = random.choice(a)
+    b = b + d
+
+print(f"生成的密码为【{b}】,长度是{c}个字符")
 
 # 912zPspO5DgS
+# Apple密码：iVaoPDSm04uv
