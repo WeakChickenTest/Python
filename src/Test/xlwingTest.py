696bbcb45947b08b7b11f5c6ab856aaf0a13d97f
文晋国 <wenjinguo@china.cn>
2022-10-09 09:51:54 +0800

    优化xlwings的使用

--- src/Test/xlwingTest.py
+++ src/Test/xlwingTest.py
@@ -5,44 +5,35 @@ import xlwings
 excel = xlwings.App(visible=True, add_book=False)
 excel.display_alerts = False
 excel.screen_updating = False
-"""
-# Excel文件
-filePath = r'../测试表格.xls'
 
+"""
 # 打开Excel文件
-wb1 = excel.books.open(filePath)
+wb = excel.books.open(filePath)
 # 保存Excel文件
-wb1.save()
+wb.save()
 # 关闭Excel文件
-wb1.close()
+wb.close()
 # 结束程序
 excel.quit()
+"""
 
 # 新建Excel文件，保存为testExcel.xlsx，保存在E:/Python/src目录下
-wb2 = excel.books.add()
-
-wb2.save(r'E:/Python/src/testExcel.xlsx')
-wb2.close()
-excel.quit()
+wb1 = excel.books.add()
+wb1.save(r'E:/Python/src/杂七杂八/testExcel.xlsx')
+wb1.close()
 print('Excel表格创建成功！')
 
-"""
-# 新建Excel件，在sheet1的A1单元格中填充数据
-wb3 = excel.books.add()
-wb3.sheets['Sheet1'].range('A1').value = '人生苦短'
-wb3.sheets['Sheet1'].range('A2').value = '我用Python'
-
-
-wb3.save(r'e:/Python/src/testExcel1.xlsx')
-wb3.close()
+# 新建Excel文件，在sheet1的A1和A2单元格中填充数据
+wb2 = excel.books.add()
+wb2.sheets['Sheet1'].range('A1').value = '人生苦短'
+wb2.sheets['Sheet1'].range('A2').value = '我用Python'
+wb2.save(r'e:/Python/src/杂七杂八/testExcel1.xlsx')
+wb2.close()
 print('Sheet1的单元格填充成功！')
 
-
-wb = excel.books.open(r"e:/Python/src/testExcel1.xlsx")
-wb.sheets['Sheet1'].range('A3').value = '你用什么?'
-
-wb.save()
-wb.close()
+# 填充testExcel1.xlsx文件sheet1中的A3单元格
+wb3 = excel.books.open(r"e:/Python/src/杂七杂八/testExcel1.xlsx")
+wb3.sheets['Sheet1'].range('A3').value = '你用什么?'
+wb3.save()
+wb3.close()
 excel.quit()
-
-
